82c1068a82a20bb092b7bd8160cb556077313d7c
Sujay PATIL <Sujay@MacBook-Pro-de-Sujay.local>
2018-11-12 21:54:05 +0100

    description component centered

--- src/App.js
+++ src/App.js
@@ -4,18 +4,13 @@ import Signin from './signin.js';
 import Signup from './signup.js';
 import Favoris from './favoris.js';
 import LandingPage from './landingPage.js'
+import Description from './description.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-
-
-
-        </header>
-      </div>
+       
     );
   }
 }
--- src/description.js
+++ src/description.js
@@ -7,55 +7,51 @@ import './description.css';
 class Description extends Component {
   render() {
     return (
-      <div className="focus">
-      <Container>
-        <Row className="heading">
-          <Col><FontAwesomeIcon icon={faCity}/></Col>
-          <Col>Parc Monceau</Col>
-          <Col><FontAwesomeIcon icon={faTimesCircle}/></Col>
+        <Container className="description">
+        <Container className="heading">
+          <Row >
+            <Col xs="12" sm="4"><FontAwesomeIcon icon={faCity} className="iconSize"/></Col>
+            <Col xs="12" sm="4">Parc Monceau</Col>
+            <Col xs="12" sm="4"><FontAwesomeIcon icon={faTimesCircle} className="iconSize"/></Col>
+          </Row>
+        </Container>
+        <Container className="descriptionBody">
+          <Row>
+            <Col xs="12">Date d'Observation: 13.10.2018</Col>
+          </Row>
+          <Row>
+            <Col xs="12">Latitude: 48.879684</Col>
+          </Row>
+          <Row>
+            <Col xs="12">Longitude: 2.308955</Col>
+          </Row>
+          <Row>
+            <Col xs="12">Horizon sud dégagé: sud à sud-ouest</Col>
+          </Row>
+          <Row>
+            <Col xs="12">C9 = Ciel de centre-ville : Les seuls objets célestes qui offrent de belles images au télescope sont la Lune, les planètes, et certains des amas d'étoiles les plus brillants (à condition qu'on puisse les localiser). La magnitude limite à l'œil nu est 4,0 ou moins.</Col>
+          </Row>
+          <Row>
+            <Col xs="12" sm="6"><FontAwesomeIcon icon={faSun} className="weatherIcon"/></Col>
+            <Col xs="12" sm="6">
+              <Row>Météo actuelle</Row>
+              <Row>Ciel dégagé</Row>
+              <Row>25° C</Row>
+              <Row>Brise légère, 2.6 m/s</Row>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs="12">Observation planétaire et lunaire uniquement</Col>
+          </Row>
+          <Row>
+            <Col xs="12">Compromis urbain</Col>
+          </Row>
+        <Row className="icons">
+          <FontAwesomeIcon xs="12" sm="6" icon={faPlusCircle} id="plusIcon"/>
+          <FontAwesomeIcon xs="12" sm="6" icon={faHeart} id="loveIcon"/>
         </Row>
-        <div className="descriptionBody">
-        <Row>
-          <Col>Date d'observation: 13.10.2018</Col>
-        </Row>
-        <Row>
-          <Col>Latitude: 48.879684</Col>
-        </Row>
-        <Row>
-          <Col>Longitude: 2.308955</Col>
-        </Row>
-        <Row>
-          <Col>Horizon sud dégagé: sud à sud-ouest</Col>
-        </Row>
-        <Row>
-          <Col>Echelle de Bortle: C9</Col>
-        </Row>
-        <Row>
-          <Col>C9 = Ciel de centre-ville : Les seuls objets célestes qui offrent de belles images au télescope sont la Lune, les planètes, et certains des amas d'étoiles les plus brillants (à condition qu'on puisse les localiser). La magnitude limite à l'œil nu est 4,0 ou moins.</Col>
-        </Row>
-        <Row>
-          <Col><FontAwesomeIcon icon={faSun}/></Col>
-          <Col>
-            <Row>Météo Actuelle</Row>
-            <Row>Ciel dégagé</Row>
-            <Row>25° C</Row>
-            <Row>Brise légère, 2.6 m/s</Row>
-          </Col>
-        </Row>
-        <Row>
-          <Col>Observation planétaire et lunaire uniquement</Col>
-        </Row>
-        <Row>
-          <Col>Compromis urbain</Col>
-        </Row>
-        <Row>
-          <Col><FontAwesomeIcon icon={faPlusCircle} size={70}/></Col>
-          <Col><FontAwesomeIcon icon={faHeart}/></Col>
-        </Row>
-        </div>
-      </Container>
-      </div>
-
+        </Container>
+        </Container>
 
     );
   }
