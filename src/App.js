93a2fad02cc13c504c167c97663fdb6606b7c784
Florian Merle <merle.florian.84@gmail.com>
2018-11-12 16:32:49 +0100

    map and geolocation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {  } from 'reactstrap';
 import logo from './logo.svg';
+import GoogleApiWrapper from './Map.js'
 import './App.css';
 
 class App extends Component {
@@ -8,18 +9,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+          <GoogleApiWrapper />
+
         </header>
       </div>
     );
