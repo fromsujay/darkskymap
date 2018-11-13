eb35c45abd15461ebfb853aa21c02f4b5eff9cbc
Sujay PATIL <Sujay@MacBook-Pro-de-Sujay.local>
2018-11-13 10:14:41 +0100

    import Google Maps Wrapper in App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import {  } from 'reactstrap';
 import Signin from './signin.js';
 import Signup from './signup.js';
 import Favoris from './favoris.js';
-import LandingPage from './landingPage.js'
-import Description from './description.js'
-import Details from './details.js'
+import LandingPage from './landingPage.js';
+import Description from './description.js';
+import Details from './details.js';
+import GoogleApiWrapper from './Map.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-       <Details/>
+       
     );
   }
 }
