f7d167a7d120dcbb13012f3f1156441c241b9301
Sujay PATIL <Sujay@MacBook-Pro-de-Sujay.local>
2018-11-16 17:18:55 +0100

    navigation description details

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Signin from './component/signin.js';
 import Signup from './component/signup.js';
 import Favoris from './component/favoris.js';
 import Map from './component/Map.js';
-import Description from './component/description.js';
-import Details from './component/details.js';
 import LandingPage from './component/landingPage.js';
 import GoogleApiWrapper from './component/Map.js';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
@@ -21,8 +19,6 @@ class App extends Component {
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route path="/map" component={Map} />
-        <Route path="/details" component={Details} />
-        <Route path="/description" component={Description} />
         <Route path="/signin" component={Signin} />
         <Route path="/signup" component={Signup} />
         <Route path="/favoris" component={Favoris} />
